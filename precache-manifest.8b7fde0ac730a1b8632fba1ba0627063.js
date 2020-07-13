5d314c7ed308044d94f576f7c2c05b64dbeda110
radmir-shkred <psitecon@gmail.com>
2020-07-13 15:38:24 +0300

    Updates

---
+++ precache-manifest.8b7fde0ac730a1b8632fba1ba0627063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a8850272b6202a68e56b09c99039a1",
+    "url": "/react-youtube-todolist-hooks/index.html"
+  },
+  {
+    "revision": "f8e843d88b26d0c9ec92",
+    "url": "/react-youtube-todolist-hooks/static/css/main.8698c192.chunk.css"
+  },
+  {
+    "revision": "c739f6ddeabb13c8f428",
+    "url": "/react-youtube-todolist-hooks/static/js/2.e4a7880a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube-todolist-hooks/static/js/2.e4a7880a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488b62411f533a880df7",
+    "url": "/react-youtube-todolist-hooks/static/js/3.6525be3f.chunk.js"
+  },
+  {
+    "revision": "f8e843d88b26d0c9ec92",
+    "url": "/react-youtube-todolist-hooks/static/js/main.e430776d.chunk.js"
+  },
+  {
+    "revision": "c5429e9b3605fcc5ac96",
+    "url": "/react-youtube-todolist-hooks/static/js/runtime-main.88263944.js"
+  }
+]);
